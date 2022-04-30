84c93c80c2a0665a1e767de46f062bf824100be4
Miguel <miguelferlez@gmail.com>
2022-04-30 16:34:59 +0200

    Add splash screen before loading app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import {View,Text} from 'react-native';
-function App(){
-  return(
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-      <Text>Hello Todoom!</Text>
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+export default function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+
+  return (
+    <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
+      <Text style={{ fontFamily: 'VeniceClassic', color: 'rgb(46,46,56)', fontSize: 60 }}>Header</Text>
+      <Text style={{ fontFamily: 'ZillaSlab-Medium', color: 'rgb(46,46,56)', fontSize: 18 }}>Body sample text</Text>
     </View>
   )
 }
-export default App;
