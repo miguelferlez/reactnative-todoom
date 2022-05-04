4211abb9b78f2f5d92209b0e29a5a8769ce667ed
Miguel <miguelferlez@gmail.com>
2022-05-04 12:28:46 +0200

    Update exit screen to handle dark mode enabled

--- src/navigation/Exit.js
+++ src/navigation/Exit.js
@@ -1,17 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, BackHandler } from "react-native";
-import { headerList } from "../styles/Typography";
-import { centered } from "../styles/Containers";
+import { body, bodyDarkMode } from "../styles/Typography";
+import { centered, container, containerDarkMode } from "../styles/Containers";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Exit() {
 
+    const [colorScheme, setColorScheme] = useState(null);
+
     useEffect(() => {
+        AsyncStorage.getItem('isDarkMode').then((value) => {
+            if (value == null || value == '0') {
+                setColorScheme('light');
+            } else {
+                setColorScheme('dark');
+            }
+        });
         BackHandler.exitApp();
     }, []);
 
     return (
-        <View style={centered}>
-            <Text style={headerList}>...</Text>
+        <View style={colorScheme === 'light' ? [container,centered] : [containerDarkMode,centered]}>
+            <Text style={colorScheme === 'light' ? bodyDarkMode : body}>Cargando...</Text>
         </View>
     );
 }
