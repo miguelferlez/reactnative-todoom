f229b4441b39b500fac1dac5642f450bbef7408a
Miguel <miguelferlez@gmail.com>
2022-05-04 12:39:12 +0200

    Update about screen and stack to handle dark mode enabled

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,44 +1,68 @@
-import React from "react";
-import { View, Text, ScrollView, TouchableOpacity, Linking } from "react-native";
-import { centered, container, linkIcon, linkText, paragraph } from "../styles/Containers";
-import { GithubBlack } from "../styles/Icons";
-import {  headerTitle, bodyInfo, bodyInfoItalic, bodyInfoBold } from "../styles/Typography";
+import React, { useEffect, useState } from "react";
+import { View, Text, ScrollView, TouchableOpacity, Linking, Appearance } from "react-native";
+import { centered, container, containerDarkMode, linkIcon, linkText, paragraph } from "../styles/Containers";
+import { GithubBlack, GithubWhite } from "../styles/Icons";
+import { headerTitle, bodyInfo, bodyInfoItalic, bodyInfoBold, headerTitleDarkMode, bodyInfoDarkMode, bodyInfoBoldDarkMode } from "../styles/Typography";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function AboutScreen() {
 
-    return (
-        <ScrollView style={container}>
-                <Text style={[paragraph, headerTitle]}>¿Qué es Todoom?</Text>
-                <Text style={[paragraph, bodyInfo]}>
-                    Todoom es la aplicación para anotar y ponerse al día con las tareas que tú, amante 
-                    de los videojuegos y nostálgico de aquellas tardes frente a la recreativa, estabas 
+    const [isLoading, setIsLoading] = useState(true);
+    const [colorScheme, setColorScheme] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        AsyncStorage.getItem('isDarkMode').then((value) => {
+            if (value == null || value == '0') {
+                setColorScheme('light');
+            } else {
+                setColorScheme('dark');
+            }
+            setIsLoading(false);
+        })
+    }, []);
+
+    if (isLoading) {
+        return (
+            <View style={centered}>
+                <Text>...</Text>
+            </View>
+        );
+    } else {
+        return (
+            <ScrollView style={colorScheme === 'light' ? container : containerDarkMode}>
+                <Text style={colorScheme === 'light' ? [paragraph, headerTitle] : [paragraph, headerTitleDarkMode]}>¿Qué es Todoom?</Text>
+                <Text style={colorScheme === 'light' ? [paragraph, bodyInfo] : [paragraph, bodyInfoDarkMode]}>
+                    Todoom es la aplicación para anotar y ponerse al día con las tareas que tú, amante
+                    de los videojuegos y nostálgico de aquellas tardes frente a la recreativa, estabas
                     esperando tener en tu móvil.
                 </Text>
-                <Text style={[paragraph, bodyInfo]}>
-                    Tan sencillo como entrar en la app, pulsar el botón para crear una nueva tarea, 
-                    rellenar toda la información que se necesite: una breve descripción, fecha y hora 
-                    y ¡listo, ya tienes tu nueva tarea en la ventana principal! Eso sí, ¡olvides marcarla 
+                <Text style={colorScheme === 'light' ? [paragraph, bodyInfo] : [paragraph, bodyInfoDarkMode]}>
+                    Tan sencillo como entrar en la app, pulsar el botón para crear una nueva tarea,
+                    rellenar toda la información que se necesite: una breve descripción, fecha y hora
+                    y ¡listo, ya tienes tu nueva tarea en la ventana principal! Eso sí, ¡olvides marcarla
                     cuando termines!.
                 </Text>
-                <Text style={[paragraph, headerTitle]}>¿Por qué Todoom?</Text>
-                <Text style={[paragraph, bodyInfo]}>
-                    En efecto, el juego de palabras es horrible, pero oye, da en el clavo, ¿no?. La idea es 
-                    combinar las típicas herramientas <Text style={bodyInfoItalic}>To-Do</Text> con un toque más 
-                    a nuestro gusto, haciendo uso de elementos de estilo <Text style={bodyInfoItalic}>pixel art </Text>  
-                    para dar esa temática distintiva del resto. ¡Y qué mejor cómplice para acompañar esta idea que 
+                <Text style={colorScheme === 'light' ? [paragraph, headerTitle] : [paragraph, headerTitleDarkMode]}>¿Por qué Todoom?</Text>
+                <Text style={colorScheme === 'light' ? [paragraph, bodyInfo] : [paragraph, bodyInfoDarkMode]}>
+                    En efecto, el juego de palabras es horrible, pero oye, da en el clavo, ¿no?. La idea es
+                    combinar las típicas herramientas <Text style={bodyInfoItalic}>To-Do</Text> con un toque más
+                    a nuestro gusto, haciendo uso de elementos de estilo <Text style={bodyInfoItalic}>pixel art </Text>
+                    para dar esa temática distintiva del resto. ¡Y qué mejor cómplice para acompañar esta idea que
                     un clásico por excelencia como es el videojuego <Text style={bodyInfoItalic}>Doom</Text>!.
                 </Text>
-                <Text style={[paragraph, headerTitle]}>¿Y quién soy yo?</Text>
-                <Text style={[paragraph, bodyInfo]}>
-                    ¿A quién se le ha podido ocurrir un chiste tan malo? ¡Pues a mi, Miguel!, un estudiante 
-                    de Sevilla, España, que busca entrar de lleno en el mundo del desarrollo sin olvidar 
+                <Text style={colorScheme === 'light' ? [paragraph, headerTitle] : [paragraph, headerTitleDarkMode]}>¿Y quién soy yo?</Text>
+                <Text style={colorScheme === 'light' ? [paragraph, bodyInfo] : [paragraph, bodyInfoDarkMode]}>
+                    ¿A quién se le ha podido ocurrir un chiste tan malo? ¡Pues a mi, Miguel!, un estudiante
+                    de Sevilla, España, que busca entrar de lleno en el mundo del desarrollo sin olvidar
                     aquello que siempre le ha apasionado y acompañado en la vida, ¡los videojuegos!.
                 </Text>
                 <TouchableOpacity style={[centered, linkIcon]} onPress={() => Linking.openURL('https://github.com/miguelferlez/reactnative-todoom')}>
-                    <GithubBlack/>
-                    <Text style={[bodyInfoBold, linkText]}>Repositorio de la aplicación</Text>
+                    {colorScheme === 'light' ? <GithubBlack /> : <GithubWhite />}
+                    <Text style={colorScheme === 'light' ? [bodyInfoBold, linkText] : [bodyInfoBoldDarkMode, linkText]}>Repositorio de la aplicación</Text>
                 </TouchableOpacity>
-                <View style={{marginBottom:45}} />
-        </ScrollView>
-    );
+                <View style={{ marginBottom: 45 }} />
+            </ScrollView>
+        );
+    }
 }
--- src/navigation/stacks/AboutStack.js
+++ src/navigation/stacks/AboutStack.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AboutScreen from '../../containers/About';
 import * as Color from '../../styles/Colors';
-import { headerTitle } from '../../styles/Typography';
-import { TouchableOpacity } from 'react-native';
+import { headerTitle, headerTitleDarkMode } from '../../styles/Typography';
+import { TouchableOpacity, Appearance } from 'react-native';
 import { BackBlack, BackWhite, DropDownBlack, DropDownWhite } from '../../styles/Icons';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
 export default function AboutStack({ navigation }) {
+
+    const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+    useEffect(() => {
+        AsyncStorage.getItem('isDarkMode').then((value) => {
+            if (value == null || value == '0') {
+                setColorScheme('light');
+            } else {
+                setColorScheme('dark');
+            }
+        })
+    }, [colorScheme]);
+
     return (
         <Stack.Navigator>
             <Stack.Screen name="Sobre nosotros" component={AboutScreen} 
-                options={{
+                options={colorScheme === 'light' ? {
                     headerTitleAlign: 'center', 
-                    headerStyle: { 
-                        backgroundColor: Color.white 
-                    }, 
-                    headerTitleStyle: {
-                        ...headerTitle,
-                    },
+                    headerStyle: { backgroundColor: Color.white }, 
+                    headerTitleStyle: { ...headerTitle },
                     headerShadowVisible: false,
                     headerLeft: () => {
                         return (
@@ -35,7 +44,28 @@ export default function AboutStack({ navigation }) {
                             </TouchableOpacity>
                         );
                     },                  
-                }} 
+                }:
+                {
+                    headerTitleAlign: 'center', 
+                    headerStyle: { backgroundColor: Color.blackRaisin }, 
+                    headerTitleStyle: { ...headerTitleDarkMode },
+                    headerShadowVisible: false,
+                    headerLeft: () => {
+                        return (
+                            <TouchableOpacity onPress={() => navigation.navigate('Mis tareas')}>
+                                <BackWhite />
+                            </TouchableOpacity>
+                        );
+                    },
+                    headerRight: () => {
+                        return (
+                            <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                                <DropDownWhite />
+                            </TouchableOpacity>
+                        );
+                    },
+                }
+            } 
             />
         </Stack.Navigator>
     )
